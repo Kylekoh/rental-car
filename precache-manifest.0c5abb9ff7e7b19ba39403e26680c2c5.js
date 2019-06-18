a90a2b0afdadab1f05bc17273f9fc9711016af83
고영일 <kyle_koh@goyeong-il-ui-MacBook-Pro.local>
2019-06-18 11:51:23 +0900

    Updates

---
+++ precache-manifest.0c5abb9ff7e7b19ba39403e26680c2c5.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3179799596fe0d4245222e48781e6c2c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e5b5b138602fe56503be",
+    "url": "/static/css/main.d84fe063.chunk.css"
+  },
+  {
+    "revision": "9eee67cf782a38d35aeb",
+    "url": "/static/js/2.d6a534e5.chunk.js"
+  },
+  {
+    "revision": "e5b5b138602fe56503be",
+    "url": "/static/js/main.4fb44326.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "e7d92a8433067bb62db6c7b4b15b6ec7",
+    "url": "/static/media/benz.e7d92a84.jpeg"
+  },
+  {
+    "revision": "7f07800365488bd95b921361b990d577",
+    "url": "/static/media/corando.7f078003.jpeg"
+  },
+  {
+    "revision": "d0e3f3e627c137ca3b4786cc4ee9eee0",
+    "url": "/static/media/elec.d0e3f3e6.jpeg"
+  },
+  {
+    "revision": "87a1dccda6d005f7f48bbdc80ffa4858",
+    "url": "/static/media/genesis.87a1dccd.jpeg"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "b8c38c6931862a124ff589e875163609",
+    "url": "/static/media/main_banner(mob).b8c38c69.jpeg"
+  },
+  {
+    "revision": "3605c87e065a2c2dd119c1bde9179cd4",
+    "url": "/static/media/qm.3605c87e.jpeg"
+  },
+  {
+    "revision": "2e81f8dbecd337386d81956a5049e12d",
+    "url": "/static/media/santafe.2e81f8db.jpeg"
+  },
+  {
+    "revision": "fc405ec3d5826ca28cf97f279bc08f86",
+    "url": "/static/media/smsuv.fc405ec3.jpeg"
+  },
+  {
+    "revision": "13012f10a346d3420471cef5d33cfe01",
+    "url": "/static/media/sonata.13012f10.jpeg"
+  },
+  {
+    "revision": "4d1d8110edd1d9540f9476a5d8bb660e",
+    "url": "/static/media/spark.4d1d8110.jpeg"
+  },
+  {
+    "revision": "03f9a418a2508ade3f874ab43d150c32",
+    "url": "/static/media/suv.03f9a418.jpeg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/rental-car/precache-manifest.9ebc7ddf14a31ee4feabeda0f0569387.js"
+  "/precache-manifest.0c5abb9ff7e7b19ba39403e26680c2c5.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/rental-car/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
