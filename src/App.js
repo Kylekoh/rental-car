418d50299ba2f18ed6e997778898a8b64f282e9e
고영일 <kyle_koh@goyeong-il-ui-MacBook-Pro.local>
2019-06-20 16:18:56 +0900

    fix layout

--- src/App.js
+++ src/App.js
@@ -14,8 +14,8 @@ function App() {
     <div className="App">
     <Header></Header>
     <Main></Main>
-    <Gift></Gift>
     <CarList></CarList>
+    <Gift></Gift>
     <Tenping></Tenping>
     </div>
   );
--- src/components/CarList.js
+++ src/components/CarList.js
@@ -9,68 +9,40 @@ class CarList extends Component {
                 <ul className="list_container">
                     <li id="car_list">
                         <a href="#tenping">
+                            <img src={require('../images/car/genesis.jpeg')} alt="genesis" />
+                            <Countdown date="2019-07-01T16:33:00"></Countdown>
+                        </a>
+                    </li>
+                    <li id="car_list">
+                        <a href="#tenping">
                             <img src={require('../images/car/santafe.jpeg')} id="santafe" alt="santafe" />
                             <Countdown date="2019-06-29T16:33:00"></Countdown>
-                            <button className="cal_price">다이렉트특가확인</button>
                         </a>
                     </li>
                     <li id="car_list">
                         <a href="#tenping">
                             <img src={require('../images/car/sonata.jpeg')} alt="sonata" />
                             <Countdown date="2019-06-29T16:33:00"></Countdown>
-                            <button className="cal_price">다이렉트특가확인</button>
                         </a>
                     </li>
-                    <li id="car_list">
-                        <a href="#tenping"> 
-                            <img src={require('../images/car/genesis.jpeg')} alt="genesis" />
-                            <Countdown date="2019-07-01T16:33:00"></Countdown>
-                            <button className="cal_price">다이렉트특가확인</button>
-                        </a>
-                    </li>
+
                     <li id="car_list">
                         <a href="#tenping">
                             <img src={require('../images/car/benz.jpeg')} alt="benz" />
                             <Countdown date="2019-07-01T16:33:00"></Countdown>
-                            <button className="cal_price">다이렉트특가확인</button>
                         </a>    
                     </li>
                     <li id="car_list">
                         <a href="#tenping">
                             <img src={require('../images/car/suv.jpeg')} alt="suv" />
                             <Countdown date="2019-06-29T16:33:00"></Countdown>
-                            <button className="cal_price">다이렉트특가확인</button>
                         </a>
                     </li>
-                    <li id="car_list">
-                        <a href="#tenping">
-                            <img src={require('../images/car/spark.jpeg')} alt="spark" />
-                            <Countdown date="2019-06-28T16:33:00"></Countdown>
-                            <button className="cal_price">다이렉트특가확인</button>
-                        </a>
-                    </li>
-                    <li id="car_list">
-                        <a href="#tenping">
-                            <img src={require('../images/car/smsuv.jpeg')} alt="smsuv" />
-                            <Countdown date="2019-06-28T16:33:00"></Countdown>
-                            <button className="cal_price">다이렉트특가확인</button>
-                        </a>
-                    </li>
-                    <li id="car_list">
-                        <a href="#tenping">
-                            <img src={require('../images/car/elec.jpeg')} alt="elec" />
-                            <Countdown date="2019-06-29T16:33:00"></Countdown>
-                            <button className="cal_price">다이렉트특가확인</button>
-                        </a>
-                    </li>
-                    <li id="car_list">
-                        <a href="#tenping">
-                            <img src={require('../images/car/corando.jpeg')} alt="corando" />
-                            <Countdown date="2019-06-29T16:33:00"></Countdown>
-                            <button className="cal_price">다이렉트특가확인</button>
-                        </a>
-                    </li>
+                    <a href="#tenping">    
+                        <button className="cal_price">핫딜가격 상담받기</button>
+                    </a>
                 </ul>
+                
         );
     }
 }
